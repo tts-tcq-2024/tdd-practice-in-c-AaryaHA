e2140c989420f1110798163c48f50f5295cb067f
AaryaHA <haprasad2000@gmail.com>
2024-07-11 18:35:48 +0530

    isemptystring

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,6 +1,16 @@
+boolean isemptystring(const char* input)
+{
+if (string == NULL || string[0] == '\0')
+  {
+        return TRUE; // The string is empty
+  }
+  return FALSE;
+}
+
+
 int add(const char* string)
 {
-  if (string == NULL || string[0] == '\0')
+  if (isemptystring==TRUE)
   {
         return 0; // The string is empty
   }
